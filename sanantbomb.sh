cfd7d870bf83c7cde94bfd68f84b9604180941d4
sananakther9740 <61096842+sananakther9740@users.noreply.github.com>
2020-02-17 03:00:36 +0530

    Update and rename TBomb.sh to sanantbomb.sh

--- sanantbomb.sh
+++ sanantbomb.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 clear
-echo -e "\e[4;31m SpeedX Productions !!! \e[0m"
+echo -e "\e[4;31m Sanan Productions !!! \e[0m"
 echo -e "\e[1;34m Presents \e[0m"
-echo -e "\e[1;32m TBomb \e[0m"
+echo -e "\e[1;32m sanantbomb \e[0m"
 echo "Press Enter To Continue"
 read a1
-if [[ -s update.speedx ]];then
+if [[ -s update.sanan ]];then
 echo "All Requirements Found...."
 else
 echo 'Installing Requirements....'
@@ -14,7 +14,7 @@ echo .
 apt install figlet toilet python curl -y
 apt install python3-pip
 pip install -r requirements.txt
-echo This Script Was Made By SpeedX >update.speedx
+echo This Script Was Made By Sanan >update.sanan
 echo Requirements Installed....
 echo Press Enter To Continue...
 read upd
@@ -26,11 +26,11 @@ clear
 echo -e "\e[1;31m"
 figlet TBomb
 echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f mono12 -F border SpeedX
-echo -e "\e[4;34m This Bomber Was Created By SpeedX \e[0m"
+toilet -f mono12 -F border Sanan
+echo -e "\e[4;34m This Bomber Was Created By Sanan \e[0m"
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
+echo -e "\e[1;32m           Mail: sananakther@gmail.com \e[0m"
+echo -e "\e[4;32m   YouTube Page: sorry no youtube page \e[0m"
 echo " "
 echo -e "\e[4;31m Please Read Instruction Carefully !!! \e[0m"
 echo " "
@@ -50,7 +50,7 @@ exit 0
 elif [ $ch -eq 2 ];then
 clear
 echo -e "\e[1;32m"
-echo 'Call Bomb By SpeedX'> call.xxx
+echo 'Call Bomb By Sanan'> call.xxx
 python3 bomber.py call
 rm *.xxx >/dev/null 2>&1
 exit 0
@@ -58,17 +58,17 @@ elif [ $ch -eq 3 ];then
 clear
 apt install git -y
 echo -e "\e[1;34m Downloading Latest Files..."
-git clone https://github.com/TheSpeedX/TBomb
-if [[ -s TBomb/TBomb.sh ]];then
-cd TBomb
+git clone https://github.com/sananakther9740/sanantbomb
+if [[ -s sanantbomb/sanantbomb.sh ]];then
+cd sanantbomb
 cp -r -f * .. > temp
 cd ..
-rm -rf  TBomb >> temp
-rm update.speedx >> temp
+rm -rf  sanantbomb >> temp
+rm update.sanan >> temp
 rm temp
-chmod +x TBomb.sh
+chmod +x sanantbomb.sh
 fi
-echo -e "\e[1;32m TBomb Will Restart Now..."
+echo -e "\e[1;32m sanantbomb Will Restart Now..."
 echo -e "\e[1;32m All The Required Packages Will Be Installed..."
 echo -e "\e[1;34m Press Enter To Proceed To Restart..."
 read a6
@@ -77,9 +77,9 @@ exit
 elif [ $ch -eq 4 ];then
 clear
 echo -e "\e[1;33m"
-figlet TBomb
+figlet sanantbomb
 echo -e "\e[1;34mCreated By \e[1;34m"
-toilet -f mono12 -F border SpeedX
+toilet -f mono12 -F border Sanan
 echo  " "
 echo -e "\e[1;32m                   Features\e[1;34m"
 echo "  [+] Unlimited And Super-Fast Bombing"
@@ -89,14 +89,14 @@ echo "  [+] Protection List"
 echo "  [+] Automated Future Updates"
 echo "  [+] Easy To Use And Embed in Code"
 echo -e "\e[1;32m                   Contributors\e[1;33m"
-echo -e "\e[1;33m      [*]  SpeedX   \e[1;31m"
-echo "         [-] Mail At: ggspeedx29@gmail.com"
-echo -e "\e[1;33m      [*]  The Black Hacker Roxstar   \e[1;31m"
-echo "         [-] Ping At: http://wa.me/917600140353"
-echo -e "\e[1;33m      [*]  Rieltar   \e[1;31m"
-echo "         [-] Ping At: https://t.me/Rieltar"
-echo -e "\e[1;33m      [*]  0n1cOn3 (Stefan)   \e[1;31m"
-echo "         [-] Mail At: 0n1cOn3@gmx.ch"
+echo -e "\e[1;33m      [*]  sanan   \e[1;31m"
+echo "         [-] Mail At: sananakther@gmail.com"
+echo -e "\e[1;33m      [*]  The sanan Hacker Roxstar   \e[1;31m"
+echo "         [-] Ping At: nothing"
+echo -e "\e[1;33m      [*]  sanan   \e[1;31m"
+echo "         [-] Ping At: nothing"
+echo -e "\e[1;33m      [*]  xxxxxxx   \e[1;31m"
+echo "         [-] Mail At: xxxxxxxx"
 echo ""
 echo ""
 echo -e "\e[1;31m This Script is Only For Educational Purposes or To Prank.\e[0m"
@@ -106,9 +106,9 @@ echo -e "\e[1;32m Make Sure To Update it If It Does not Work.\e[0m"
 echo  " "
 echo -e "\e[4;31m That's All !!!\e[0m"
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
-echo -e "\e[1;32m       Whatsapp: https://bit.do/speedxgit \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
+echo -e "\e[1;32m           Mail: sananakther@gmail.com \e[0m"
+echo -e "\e[1;32m       Whatsapp: nothing \e[0m"
+echo -e "\e[4;32m   YouTube Page: nothing \e[0m"
 echo "Press Enter To Go Home"
 read a3
 clear
@@ -117,11 +117,11 @@ clear
 echo -e "\e[1;31m"
 figlet TBomb
 echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f mono12 -F border SpeedX
+toilet -f mono12 -F border sanan
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
-echo -e "\e[1;32m       Whatsapp: https://bit.do/speedxgit \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
+echo -e "\e[1;32m           Mail: sananakther@gmail.com \e[0m"
+echo -e "\e[1;32m       Whatsapp: xxxxxxxx \e[0m"
+echo -e "\e[4;32m   YouTube Page: xxxxxxxxx \e[0m"
 echo " "
 exit 0
 else
